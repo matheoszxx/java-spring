c5e5c44ef0e94b09fc7255f19ace99a35320d296
matheus <matheuszxx1@outlook.com>
2023-10-21 12:12:43 -0300

    criando 'ClienteService' e 'ViaCepService'

--- src/main/java/com/example/demo/service/ClienteService.java
+++ src/main/java/com/example/demo/service/ClienteService.java
@@ -1,5 +1,24 @@
 package com.example.demo.service;
 
-public interface ClienteService {
+import com.example.demo.model.Cliente;
 
+/**
+ * Interface que define o padrão <b>Strategy</b> no dominio de cliente. Com isso, se necessário, podemos
+ * ter multiplas implementações dessa mesma interface.
+ * 
+ * 
+ * @author matheoszxx
+ */
+
+public interface ClienteService {
+	
+	Iterable<Cliente> buscarTodos();
+	
+	Cliente buscarPorId(Long id);
+	
+	void inserir(Cliente cliente);
+	
+	void atualizar(Long id, Cliente cliente);
+	
+	void deletar(Long id);
 }
--- src/main/java/com/example/demo/service/ViaCepService.java
+++ src/main/java/com/example/demo/service/ViaCepService.java
@@ -1,5 +1,23 @@
 package com.example.demo.service;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.model.Endereco;
+
+/**
+ * 
+ * Cliente HTTP, criado via <b>OpenFeign</b>, para o consumo da API do <b>ViaCEP</b>
+ * 
+ */
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
+	
+	@GetMapping("/{cep}/json/")
+	Endereco consultarCep(@PathVariable("cep") String cep);
 
 }
